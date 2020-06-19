d4f1d2935860eed25c6f0206e47179034d2e321f
Dread <Traxthunder@gmail.com>
2020-06-19 16:54:27 -0500

    Rithwith World Boss
    World Boss with landblocks. Water Golem on rithwic

---
+++ Landblock/C88D.sql
@@ -0,0 +1,33 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0xC88D;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C88D000,  6103, 0xC88D0101, 35.0909, 14.0979, 24.005, 0.999216, 0, 0, -0.0395811, False, '2005-02-09 10:00:00'); /* Rithwic Meeting Hall Portal */
+/* @teleloc 0xC88D0101 [35.090900 14.097900 24.004999] 0.999216 0.000000 0.000000 -0.039581 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C88D001, 27547, 0xC88D0000, 46.2842, 10.5845, 22.005, 0.928513, 0, 0, 0.371301, False, '2005-02-09 10:00:00'); /* Bind Stone */
+/* @teleloc 0xC88D0000 [46.284199 10.584500 22.004999] 0.928513 0.000000 0.000000 0.371301 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C88D002, 5000853, 0xC88D0021, 99.5777, 9.26356, 22.055, 0.983194, 0, 0, 0.182562, False, '2020-06-19 16:48:58'); /* RithAttack */
+/* @teleloc 0xC88D0021 [99.577698 9.263560 22.055000] 0.983194 0.000000 0.000000 0.182562 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C88D003, 5000853, 0xC88D0029, 138.677, 9.0332, 22.055, 0.725727, 0, 0, -0.687983, False, '2020-06-19 16:49:17'); /* RithAttack */
+/* @teleloc 0xC88D0029 [138.677002 9.033200 22.055000] 0.725727 0.000000 0.000000 -0.687983 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C88D004, 5000853, 0xC88D0011, 51.2855, 11.5178, 22.055, 0.990224, 0, 0, 0.139486, False, '2020-06-19 16:49:39'); /* RithAttack */
+/* @teleloc 0xC88D0011 [51.285500 11.517800 22.055000] 0.990224 0.000000 0.000000 0.139486 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C88D005, 5000853, 0xC88D0012, 59.8867, 43.0791, 18.5721, -0.99982, 0, 0, -0.0189501, False, '2020-06-19 16:49:58'); /* RithAttack */
+/* @teleloc 0xC88D0012 [59.886700 43.079102 18.572100] -0.999820 0.000000 0.000000 -0.018950 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C88D006, 5000853, 0xC88D0023, 106.664, 54.2922, 10.055, -0.868296, 0, 0, 0.496046, False, '2020-06-19 16:50:03'); /* RithAttack */
+/* @teleloc 0xC88D0023 [106.664001 54.292198 10.055000] -0.868296 0.000000 0.000000 0.496046 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C88D007, 5000853, 0xC88D003A, 168.9729, 43.741554, 11.941002, -0.4469133, 0, 0, 0.89457726, False, '2020-06-19 16:50:09'); /* RithAttack */
+/* @teleloc 0xC88D003A [168.972900 43.741554 11.941002] -0.446913 0.000000 0.000000 0.894577 */
---
+++ World Bosses/RithwicAttack/Weenies/5000853.sql
@@ -0,0 +1,38 @@
+DELETE FROM `weenie` WHERE `class_Id` = 5000853;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (5000853, 'ace-5000853', 1, '2020-05-22 10:00:00') /* Generic */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (5000853,  81,          6) /* MaxGeneratedObjects */
+     , (5000853,  82,          6) /* InitGeneratedObjects */
+     , (5000853,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */
+     , (5000853, 103,          2) /* GeneratorDestructionType - Destroy */
+     , (5000853, 142,          3) /* GeneratorTimeType - Event */
+     , (5000853, 145,          2) /* GeneratorEndDestructionType - Destroy */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (5000853,   1, True ) /* Stuck */
+     , (5000853,  11, True ) /* IgnoreCollisions */
+     , (5000853,  18, True ) /* Visibility */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (5000853,  41,      60) /* RegenerationInterval */
+     , (5000853,  43,       20) /* GeneratorRadius */
+     , (5000853, 121,      10) /* GeneratorInitialDelay */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (5000853,   1, 'RithAttack') /* Name */
+     , (5000853,  34, 'RithAttack') /* GeneratorEvent */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (5000853,   1,   33555051) /* Setup */
+     , (5000853,   8,  100667494) /* Icon */;
+
+INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (5000853, -1, 5000856, 0, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Ghost of Banderling Past (5000821) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (5000853, -1, 5000856, 0, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Ghost of Banderling Past (5000821) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (5000853, -1, 5000856, 0, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Ghost of Banderling Past (5000821) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (5000853, -1, 5000856, 0, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Ghost of Banderling Past (5000821) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (5000853, -1, 5000856, 0, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Ghost of Banderling Past (5000821) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+     , (5000853, -1, 5000856, 0, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Ghost of Banderling Past (5000821) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
