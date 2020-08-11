771ab6b0e99e43f25542e55a4d545edac6aef506
Dread Privateer <65474632+traxthunder@users.noreply.github.com>
2020-08-11 02:24:55 -0400

    Children fixes

--- Trial/weenies/2000205 Child.sql
+++ Trial/weenies/2000205 Child.sql
@@ -70,8 +70,7 @@ INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (2000205,   1, 'Child') /* Name */
      , (2000205,   4, 'Aluvian') /* HeritageGroup */;
 
-INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
-VALUES 
+
 INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
 VALUES (2000205,   1,   33554433) /* Setup */
      , (2000205,   2,  150994945) /* MotionTable */
