10bfb07a91ef4987175864da926d4aa1ea3ab6aa
Mike Talley <miketalley@hotmail.com>
2020-08-11 23:00:14 -0400

    added missing semicolon to Tink event sql for generating ghost of banderling past

--- Tink event/5000298.sql
+++ Tink event/5000298.sql
@@ -30,4 +30,4 @@ VALUES (5000298,   1,   33555051) /* Setup */
      , (5000298,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (5000298, -1, 5000297, 0, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Ghost of Banderling Past (5000821) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
+VALUES (5000298, -1, 5000297, 0, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0); /* Generate Ghost of Banderling Past (5000821) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */
