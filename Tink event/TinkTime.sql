d5decc04c5b687cef17deae841de32c48de7bad8
Dread Privateer <65474632+traxthunder@users.noreply.github.com>
2021-02-10 19:12:00 -0500

    Update

--- Tink event/TinkTime.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `event` WHERE `name` = 'Tinktime';
-
-INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
-VALUES ('TinkTime', -1, -1, 3, '2020-05-24 00:00:00');
