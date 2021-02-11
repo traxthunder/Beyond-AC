7bbe795c730bb64b7a6ae1d8b5c29c13f3499843
Dread Privateer <65474632+traxthunder@users.noreply.github.com>
2021-02-10 19:04:28 -0500

    updates

--- Tink event/5100000.sql
+++
@@ -1,3 +0,0 @@
-
-DELETE FROM `quest` WHERE (`id` = '5100000');
-INSERT INTO `quest` (`id`, `name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`) VALUES ('5100000', 'Tink', '560000', '-1', 'Tink', '2019-09-05 19:03:38');
